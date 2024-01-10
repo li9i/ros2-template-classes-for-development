74e1a1587793411b59346cb575fb64f2ccdac8b5
alexandros filotheou <alexandros.filotheou@gmail.com>
2024-01-10 11:31:09 +0200

    Can grab service response through async callback

--- msg_srv/msg_srv_pkg_cpp/include/template_class.hpp
+++ msg_srv/msg_srv_pkg_cpp/include/template_class.hpp
@@ -38,7 +38,14 @@ class TemplateClass final : public rclcpp::Node
 
     // A service (server)
     rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr service_;
+
+    // A service client
     rclcpp::Client<std_srvs::srv::Trigger>::SharedPtr service_client_;
+
+    // A pointer to a trigger response
+    std_srvs::srv::Trigger::Response::SharedPtr service_response_ptr_;
+    bool service_response_success_;
+    std::string service_response_message_;
 
     // A timer
     rclcpp::TimerBase::SharedPtr timer_;
